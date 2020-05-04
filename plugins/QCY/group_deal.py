f285321163a4deba1529d29ec7f126f66ecc84c2
QianCY <q-young@qq.com>
2020-05-05 02:11:40 +0800

    禁用了mma和latex

--- plugins/QCY/group_deal.py
+++ plugins/QCY/group_deal.py
@@ -1,15 +1,14 @@
 from nonebot import on_notice, NoticeSession
-
-神音群={80852074}
+from config import 神音群ID
 
 @on_notice('group_increase')
 async def _(session: NoticeSession):
     # 发送欢迎消息
-    if session.event.group_id in 神音群:
+    if session.event.group_id in 神音群ID:
         await session.send('欢迎加入二次元抱抱群w')
 
 @on_notice('group_decrease')
 async def _(session: NoticeSession):
     # 发送退群消息
-    if session.event.group_id in 神音群:
+    if session.event.group_id in 神音群ID:
         await session.send(f'{session.event.user_id}小妹妹退群惹qwq')
